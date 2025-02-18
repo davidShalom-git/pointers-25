d8b6c50796c1de6366313218245c463828413abb
davidshalom-git <davidshalom2609@gmail.com>
2025-02-18 10:23:06 +0530

    CSE

--- Frontend/src/Component/Home.jsx
+++ Frontend/src/Component/Home.jsx
@@ -18,7 +18,7 @@ import explore from "../assets/Explore.png";
 import it from '../assets/SSJ.jpg';
 import cse from '../assets/R.png';
 import beerus from '../assets/Beerus.jpg';
-import broly from '../assets/broly.png';
+import broly from '../assets/Broly.png';
 import Nav from "./Nav";
 import "../App.css";
 
